b8360e631aaadd3729bd42f459b1bc7a8a96390f
Xochil73 <xochilsquaglia@yahoo.com>
2018-09-22 10:48:36 -0700

    03branch

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { var num=a+b;
     var num2="The sum of " + a + " and " + b + " is " + num + ".";
-    var answers;
-    answers = [num, num2];
+    //var answers;
+    return [num, num2];
    // console.log(num2); 
-    return answers;  //eslint-disable-line
+    //return answers;  //eslint-disable-line
 
 }
 
@@ -33,9 +33,9 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { var num3=a*b;
     var num4="The product of " + a + " and " + b + " is " + num3 + ".";
-    var answers;
-    answers=[num3, num4];
-    return answers;
+   // var answers;
+    return [num3, num4];
+    //return answers;
     //eslint-disable-line
 
 }
@@ -57,12 +57,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b, c) { 
+    var sumFunction = sum(a,b);
+    var multiSum = sum(sumFunction[0], c);
+    var isolatedSum = multiSum[0];
+    var sumMessage= a + ' and ' + b + ' and ' + c + ' sum to ' + isolatedSum + '.';
+    var productFunction = multiply(a, b);
+    var multiProduct = multiply(productFunction[0], c);
+    var isolatedProduct= multiProduct[0];
+    var productMessage = 'The product of ' + a + " and " + b + " and " + c + " is " + isolatedProduct + ".";
+    return [isolatedSum, isolatedProduct, sumMessage, productMessage];
+    
+    
+    //eslint-disable-line
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
